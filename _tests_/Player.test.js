5e120773c87612cd4e10dab6c257a9a0742e4e41
Mikeysoar <mwclimb@gmail.com>
2021-06-23 14:31:29 -0600

    code clean up to match 10.2

--- _tests_/Player.test.js
+++ _tests_/Player.test.js
@@ -1,9 +1,7 @@
 const Player = require('../lib/Player.js');
 const Potion = require('../lib/Potion.js');
 
-jest.mock('../lib/Potion');
-
-console.log(new Potion());
+jest.mock('../lib/Potion.js');
 
 test('creates a player object', () => {
   const player = new Player('Dave');
@@ -14,8 +12,7 @@ test('creates a player object', () => {
   expect(player.agility).toEqual(expect.any(Number));
 
   expect(player.inventory).toEqual(
-    expect.arrayContaining([expect.any(Object)])
-  );
+    expect.arrayContaining([expect.any(Object)]));
 });
 
 test("gets player's stats as an object", () => {
